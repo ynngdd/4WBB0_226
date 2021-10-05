da9a1011b956b9da82c198ba70234dc074aa25d7
Yasen Yalamov <37640141+ynngdd@users.noreply.github.com>
2021-10-05 16:17:43 +0200

    Create server.c

---
+++ server.c
@@ -0,0 +1,117 @@
+/*
+  BLE_Central_Device.ino
+
+  This program uses the ArduinoBLE library to set-up an Arduino Nano 33 BLE Sense 
+  as a central device and looks for a specified service and characteristic in a 
+  peripheral device. If the specified service and characteristic is found in a 
+  peripheral device, the last detected value of the on-board gesture sensor of 
+  the Nano 33 BLE Sense, the APDS9960, is written in the specified characteristic. 
+
+  The circuit:
+  - Arduino Nano 33 BLE Sense. 
+
+  This example code is in the public domain.
+*/
+
+#include <ArduinoBLE.h>
+
+const char* deviceServiceUuid = "19b10000-e8f2-537e-4f6c-d104768a1214";
+const char* deviceServiceCharacteristicUuid = "19b10001-e8f2-537e-4f6c-d104768a1214";
+
+bool gesture = 0;  
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+  
+ 
+  
+  if (!BLE.begin()) {
+    Serial.println("* Starting BLE module failed!");
+    while (1);
+  }
+  
+  BLE.setLocalName("Nano 33 BLE (Central)"); 
+  BLE.advertise();
+
+  Serial.println("Arduino Nano 33 BLE Sense (Central Device)");
+  Serial.println(" ");
+}
+
+void loop() {
+  connectToPeripheral();
+}
+
+void connectToPeripheral(){
+  BLEDevice peripheral;
+  
+  Serial.println("- Discovering peripheral device...");
+
+  do
+  {
+    BLE.scanForUuid(deviceServiceUuid);
+    peripheral = BLE.available();
+  } while (!peripheral);
+
+  Serial.println(peripheral);
+  
+  if (peripheral) {
+    Serial.println("* Peripheral device found!");
+    Serial.print("* Device MAC address: ");
+    Serial.println(peripheral.address());
+    Serial.print("* Device name: ");
+    Serial.println(peripheral.localName());
+    Serial.print("* Advertised service UUID: ");
+    Serial.println(peripheral.advertisedServiceUuid());
+    Serial.println(" ");
+    BLE.stopScan();
+    controlPeripheral(peripheral);
+  }
+}
+
+void controlPeripheral(BLEDevice peripheral) {
+  Serial.println("- Connecting to peripheral device...");
+
+  if (peripheral.connect()) {
+    Serial.println("* Connected to peripheral device!");
+    Serial.println(" ");
+  } else {
+    Serial.println("* Connection to peripheral device failed!");
+    Serial.println(" ");
+    return;
+  }
+
+  Serial.println("- Discovering peripheral device attributes...");
+  if (peripheral.discoverAttributes()) {
+    Serial.println("* Peripheral device attributes discovered!");
+    Serial.println(" ");
+  } else {
+    Serial.println("* Peripheral device attributes discovery failed!");
+    Serial.println(" ");
+    peripheral.disconnect();
+    return;
+  }
+
+  BLECharacteristic gestureCharacteristic = peripheral.characteristic(deviceServiceCharacteristicUuid);
+    
+  if (!gestureCharacteristic) {
+    Serial.println("* Peripheral device does not have gesture_type characteristic!");
+    peripheral.disconnect();
+    return;
+  } else if (!gestureCharacteristic.canWrite()) {
+    Serial.println("* Peripheral does not have a writable gesture_type characteristic!");
+    peripheral.disconnect();
+    return;
+  }
+  
+  while (peripheral.connected()) {
+
+      delay(2000);
+      gesture = !gesture;
+      
+      gestureCharacteristic.writeValue((byte)gesture);
+      Serial.println(gesture);
+  
+  }
+  Serial.println("- Peripheral device disconnected!");
+}
