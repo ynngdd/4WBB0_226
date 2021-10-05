8ca6b2c146a84b4207f3991297b77dc2a1bbc3c2
Yasen Yalamov <37640141+ynngdd@users.noreply.github.com>
2021-10-05 16:18:08 +0200

    Create periph.c

---
+++ periph.c
@@ -0,0 +1,74 @@
+/*
+  BLE_Peripheral.ino
+
+  This program uses the ArduinoBLE library to set-up an Arduino Nano 33 BLE
+  as a peripheral device and specifies a service and a characteristic. Depending
+  of the value of the specified characteristic, an on-board LED gets on.
+
+  The circuit:
+  - Arduino Nano 33 BLE.
+
+  This example code is in the public domain.
+*/
+
+#include <ArduinoBLE.h>
+
+
+const char* deviceServiceUuid = "19b10000-e8f2-537e-4f6c-d104768a1214";
+const char* deviceServiceCharacteristicUuid = "19b10001-e8f2-537e-4f6c-d104768a1214";
+
+bool gesture = 0;
+
+BLEService gestureService(deviceServiceUuid);
+BLEBoolCharacteristic gestureCharacteristic(deviceServiceCharacteristicUuid, BLERead | BLEWrite);
+
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  if (!BLE.begin()) {
+    Serial.println("- Starting BLE module failed!");
+    while (1);
+  }
+
+  BLE.setLocalName("Arduino Nano 33 BLE (Peripheral)");
+  BLE.setAdvertisedService(gestureService);
+  gestureService.addCharacteristic(gestureCharacteristic);
+  BLE.addService(gestureService);
+  gestureCharacteristic.writeValue(0);
+  BLE.advertise();
+
+  Serial.println("Nano 33 BLE (Peripheral Device)");
+  Serial.println(" ");
+}
+
+void loop() {
+  BLEDevice central = BLE.central();
+  Serial.println("- Discovering central device...");
+  delay(500);
+
+  if (central) {
+    Serial.println("* Connected to central device!");
+    Serial.print("* Device MAC address: ");
+    Serial.println(central.address());
+    Serial.println(" ");
+
+    while (central.connected()) {
+      Serial.println("centralConn");
+           // if (gestureCharacteristic.written()) {
+      gesture = gestureCharacteristic.value();
+      writeGesture(gesture);
+          //  }
+    }
+
+    Serial.println("* Disconnected to central device!");
+  }
+}
+
+void writeGesture(bool gesture) {
+  Serial.println("it's this: ");
+  Serial.println(gesture);
+
+
+}
